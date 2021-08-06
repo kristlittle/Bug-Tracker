d95548df489f67fba59ff02f28e93ff33a00f7f2
Kristoff L <50643772+kristlittle@users.noreply.github.com>
2021-08-06 18:01:31 -0400

    updates pages

--- indexapp.js
+++ indexapp.js
@@ -6,7 +6,9 @@ const signInButton = document.getElementById('signIn')
 const container = document.getElementById('container')
 
 signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
-signInButton.addEventListener('click', () => container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
+
 
 
 
