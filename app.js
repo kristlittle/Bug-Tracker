3b91c9482c53610fea9128bb06098888151a4fca
Kristoff L <50643772+kristlittle@users.noreply.github.com>
2021-08-07 02:38:50 -0400

    updates pages

--- app.js
+++ app.js
@@ -4,88 +4,22 @@
 const cards = document.querySelectorAll(".card");
 const allLists = document.querySelectorAll(".list");
 
-//card & list listener
-cards.addEventListener('dragstart', dragStart);
-cards.addEventListener('dragend', dragEnd);
+let draggableTodo = null;
 
-for (const list of allListss) {
-    cards.addEventListener("dragover", dragOver);
-    cards.addEventListener("dragenter", dragEnter);
-    cards.addEventListener("dragleave", dragLeave);
-    cards.addEventListener("drop", dragDrop);
 
+cards.forEach((card) => {
+    card.addEventListener("dragstart", dragStart);
+})
+
+function dragStart()
+{
+    draggableTodo = this;
+    setTimeout(() => {
+        this.style.display = "none";
+    }, 0);
+    console.log("dragStart");
+        
 }
-
-//DRAG FUNSTIONS
-
-//start dragging element
-function dragStart(){
-    console.log("Start");
-}
-
-//
-function dragEnd(){
-    console.log("Ended");
-}
-
-//
-function dragOver(e){
-    e.preventDefault();
-    console.log("Over");
-}
-
-//drag element entering drop target range
-function dragEnter(){
-    console.log("Ended");
-}
-
-//drag element leaving drop target range
-function dragLeave(){
-    console.log("Leave");
-}
-
-//element dropped on target
-function dragDrop(){
-    console.log("Drop");
-}
-
-
-
-const btns = document.querySelectorAll("[data-target-modal]");
-const closeModal = document.querySelectorAll(".closeModal");
-const overlay = document.getElementById("overlay");
-
-btns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        document.querySelector(btn.dataset.targetModal).classList.add("active");
-        overlay.classList.add("active");
-    });
-});
-
-closeModal.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        const modals = btn.closest(".modal");
-        modal.classList.remove("active");
-        overlay.classList.add("active");
-    });
-});
-
-//listen for overlay and close
-window.onclick = (event) => {
-    btn.addEventListener("click", () => {
-        if(event.target == overlay) {
-            const modals = document.querySelectorAll(".modal");
-            modals.forEach((modal) => modal.classList.remove("active"));
-            overlay.classList.remove("active");   
-        }     
-    })
-};
-
-
-
-
-
-
 
 
 
