d7db18d4c1b275648680b350c1a1b4ecb9224471
Kristoff L <50643772+kristlittle@users.noreply.github.com>
2021-08-08 05:18:01 -0400

    updated pages

--- app.js
+++ app.js
@@ -9,20 +9,66 @@ let draggableTodo = null;
 
 cards.forEach((card) => {
     card.addEventListener("dragstart", dragStart);
+    card.addEventListener("dragend", dragEnd);
 })
 
+//drag action started
 function dragStart()
 {
     draggableTodo = this;
     setTimeout(() => {
-        this.style.display = "none";
+        this.style.display = "block";
     }, 0);
     console.log("dragStart");
         
 }
 
+//drag action stopped/ended
+function dragEnd()
+{
+    draggableTodo = null;
+    setTimeout(() => {
+        this.style.display = "block";
+    }, 0);
+    console.log("dragEnd");
+        
+}
 
+allLists.forEach((list) => {
+    list.addEventListener("dragover", dragOver);
+    list.addEventListener("dragenter", dragEnter);
+    list.addEventListener("dragleave", dragLeave);
+    list.addEventListener("drop", dragDrop);
 
+})
+//
+//drag over new block
+function dragOver(e)
+{
+    e.preventDefault();
+    console.log("dragOver");   
+}
+
+//entereing new block
+function dragEnter()
+{
+    this.style.border = "1px dashed #fff";
+    console.log("dragEnter");   
+}
+
+//drag leaving
+function dragLeave()
+{
+    this.style.border = "none";
+    console.log("dragLeave");   
+}
+
+//dropping 
+function dragDrop()
+{
+    this.style.border = "none";
+    console.log("dragDrop");   
+}
 
 //<!--/* ** CODE SPRINT PROJECT -- BUG TRACKER  ** 
  //KRISTOFF L.  © Kristoff L. All Rights Reserved.-->
