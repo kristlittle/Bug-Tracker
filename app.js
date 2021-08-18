09e1a0b55c1fedbe32191a231cd2612734c12d3c
Kristoff L <50643772+kristlittle@users.noreply.github.com>
2021-08-17 21:44:12 -0400

    updates pages

--- app.js
+++ app.js
@@ -106,6 +106,49 @@ window.onclick = (event) => {
 }
 
 // TODO CREATION.
+const todo_submit = document.getElementById("todo_submit");
+
+todo_submit.addEventListener("click", createTodo);
+
+function createTodo() 
+{
+    const todo_div = document.createElement("div");
+    const input_val = document.getElementById("todo_input").value;
+    const txt = document.createTextNode(input_val);
+
+    todo_div.appendChild(txt);
+    todo_div.classList.add("todo");
+    todo_div.setAttribute("draggable", "true");
+
+    //SPAN CREATION
+    const span = document.createElement("span");
+    const span_txt = document.createTextNode("\u00D7");
+    span.classList.add("close");
+    span.appendChild(span_txt);
+
+    todo_div.appendChild(span);
+
+    no_status.appendChild(todo_div);
+
+    span.addEventListener("click", () => {
+        span.parentElement.style.display = "none";
+    });
+
+    todo_div.addEventListener("dragstart", dragStart);
+    todo_div.addEventListener("dragend", dragEnd);
+
+    document.getElementById("todo_input").value = "";
+    todo_form.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+const close_btns = document.querySelectorAll(".close");
+
+close_btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.style.display = "none";
+  });
+});
 
 
 
